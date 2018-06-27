d30fac19a4c4a6a1d75f32bd9525c4c6826c553f
Allan Lindqvist <allan@funrock.com>
2018-06-28 00:35:27 +0200

    versions, fix for app target file

--- ServiceFabricSdkContrib.MsBuild/packnugets.bat
+++ ServiceFabricSdkContrib.MsBuild/packnugets.bat
@@ -1,4 +1,5 @@
 dotnet restore
+dotnet publish /p:Configuration=Debug
 dotnet publish /p:Configuration=Release
 dotnet pack /p:NuspecFile=ServiceFabricSdkContrib.Applications.nuspec /p:Configuration=Release
 dotnet pack /p:NuspecFile=ServiceFabricSdkContrib.Services.nuspec /p:Configuration=Release
