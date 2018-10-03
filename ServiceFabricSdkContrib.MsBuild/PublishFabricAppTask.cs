918297bbee7920405221af6e78a644e56c6df6e0
Allan Lindqvist <allan@funrock.com>
2018-10-03 22:01:54 +0200

    moving git files

---
+++ ServiceFabricSdkContrib.MsBuild/PublishFabricAppTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Utilities;
+
+namespace ServiceFabricSdkContrib.MsBuild
+{
+	public class PublishFabricAppTask : Task
+	{
+		public string PackageLocation { get; set; }
+		public string ClusterEndPoint { get; set; }
+		public string ThumbPrint { get; set; }
+
+		public override bool Execute()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
